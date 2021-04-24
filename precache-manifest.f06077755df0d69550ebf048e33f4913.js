21ee6134d8931af394a9d3169128d5e7d1cafcf1
Rishi <rishids37@gmail.com>
2021-04-24 17:58:37 +0530

    Updates

---
+++ precache-manifest.f06077755df0d69550ebf048e33f4913.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5574cd312d66f4d755f3dc664f09995",
+    "url": "/AnrkyGram/index.html"
+  },
+  {
+    "revision": "b9aa9c454151798c5c1f",
+    "url": "/AnrkyGram/static/css/main.8a27236c.chunk.css"
+  },
+  {
+    "revision": "8d9f708f09b940fc8e20",
+    "url": "/AnrkyGram/static/js/2.3c843915.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/AnrkyGram/static/js/2.3c843915.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9aa9c454151798c5c1f",
+    "url": "/AnrkyGram/static/js/main.099f6feb.chunk.js"
+  },
+  {
+    "revision": "d5d91123b08074dc43c9",
+    "url": "/AnrkyGram/static/js/runtime-main.054b0e49.js"
+  }
+]);
